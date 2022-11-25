8a306bb8d1f0d3bfe83d09073c570ecdb569c358
Chase Lean <chaseleanrocks@gmail.com>
2022-11-25 19:03:10 +0800

    Changed word list

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import os
-
-os.chdir("C:\\Users\\Chase Lean\\Desktop\\Javascript\\47 Learn german 2")
 
 data = pd.read_excel("german_words.xlsx")
 data.to_csv("output.csv", encoding="utf-8", index=False)
